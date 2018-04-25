0d6e1c38e1aeb3862431f8051629cee0131b0e7e
jeong <uhdang@gmail.com>
2018-04-25 18:55:53 +0200

    how to access slice?

--- 03_choose_your_own_adventure/mine/main.go
+++ 03_choose_your_own_adventure/mine/main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"os"
 )
+
+type Story map[string]Chapter
+
+type Chapter struct {
+	Title     string   `json:"title"`
+	Paragraph []string `json:"story"`
+	Options   []struct {
+		Text string `json:"text"`
+		Arc  string `json:"arc"`
+	} `json:"options"`
+}
 
 func main() {
 	filename := flag.String("file", "", "input a filename you want to open")
@@ -18,20 +30,31 @@ func main() {
 	}
 	defer file.Close()
 
-	fileinfo, err := file.Stat()
+	dec := json.NewDecoder(file)
+
+	var s Story
+	err = dec.Decode(&s)
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Printf("%s", s)
 
-	filesize := fileinfo.Size()
-	buffer := make([]byte, filesize)
+	// ==============================
 
-	bytesread, err := file.Read(buffer)
-	if err != nil {
-		log.Fatal(err)
-	}
+	//fileinfo, err := file.Stat()
+	//if err != nil {
+	//log.Fatal(err)
+	//}
 
-	fmt.Println("bytes read: ", bytesread)
-	fmt.Println("bytestream to string: ", string(buffer))
+	//filesize := fileinfo.Size()
+	//buffer := make([]byte, filesize)
+
+	//bytesread, err := file.Read(buffer)
+	//if err != nil {
+	//log.Fatal(err)
+	//}
+
+	//fmt.Println("bytes read: ", bytesread)
+	//fmt.Println("bytestream to string: ", string(buffer))
 
 }
